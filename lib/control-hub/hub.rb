75fff0d452ceba973011398463c00ad496c969c0
Ari Russo <ari.russo@gmail.com>
2014-09-30 22:17:36 -0400

    comments

--- lib/control-hub/hub.rb
+++ lib/control-hub/hub.rb
@@ -5,6 +5,9 @@ module ControlHub
 
     attr_reader :config, :nodes
 
+    # @param [Hash] options
+    # @option options [File, String] :control
+    # @option options [File, String] :io
     def initialize(options = {})
       @debug = Debug.new($>)
       @nodes = {
@@ -16,19 +19,35 @@ module ControlHub
       populate_nodes
     end
 
+    # Start the hub
+    # @return [Boolean]
     def listen  
       EM.epoll
       EM.run { enable_nodes }
+      true
     end
 
+    # Get the output nodes
+    # @param [Hash] options
+    # @option options [Symbol] :type The type of node (eg :midi)
+    # @return [IO::MIDI, IO::OSC, IO::Websocket]
     def outputs(options = {})
       nodes(:output, options)
     end
 
+    # Get the input nodes
+    # @param [Hash] options
+    # @option options [Symbol] :type The type of node (eg :midi)
+    # @return [IO::MIDI, IO::OSC, IO::Websocket]
     def inputs(options = {})
       nodes(:input, options)
     end
 
+    # Get the nodes of the given direction
+    # @param [String] direction
+    # @param [Hash] options
+    # @option options [Symbol] :type The type of node (eg :midi)
+    # @return [IO::MIDI, IO::OSC, IO::Websocket]
     def nodes(direction, options = {})
       dir = @nodes[direction]
       if options[:type].nil?
@@ -41,20 +60,26 @@ module ControlHub
 
     private
 
+    # Populate all of the nodes from the spec
     def populate_nodes
-      populate_inputs
-      populate_outputs
+      populate_inputs + populate_outputs
     end
 
+    # Enable the nodes
+    # @return [Boolean]
     def enable_nodes
-      enable_outputs
-      enable_inputs
+      enable_outputs || enable_inputs
     end
 
+    # Enable the outputs
+    # @return [Boolean]
     def enable_outputs
       @nodes[:output].each(&:start)
+      true
     end
 
+    # Enable the inputs
+    # @return [Boolean]
     def enable_inputs
       @nodes[:input].each do |input|
         Thread::abort_on_exception = true
@@ -67,8 +92,11 @@ module ControlHub
           input.start
         end
       end
+      true
     end
 
+    # Populate the inputs
+    # @return [Array]
     def populate_inputs
       @nodes[:input] += @config.nodes(:input).map do |input|
         klass = config.io_class(input[:type])
@@ -76,13 +104,19 @@ module ControlHub
       end
     end
 
+    # Populate the outputs
+    # @return [Array]
     def populate_outputs
       @nodes[:output] += @config.nodes(:output).map do |output|
         klass = config.io_class(output[:type])
         klass.new(output, :debug => @debug)
       end
     end
-
+    
+    # Populate the config
+    # @param [String, File] control_path
+    # @param [String, File] io_path
+    # @return [Config]
     def populate_config(control_path, io_path)
       @config = Config.new(:control => control_path, :io => io_path)
     end
