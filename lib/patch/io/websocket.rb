72ccb36595262779163941b2f7459685473bcca0
Ari Russo <ari.russo@gmail.com>
2014-10-12 20:18:29 -0400

    comments

--- lib/patch/io/websocket.rb
+++ lib/patch/io/websocket.rb
@@ -2,19 +2,23 @@ module Patch
 
   module IO
 
+    # Websocket IO
     class Websocket
 
       attr_reader :id
 
-      def initialize(socket_info, options = {})
-        @config = socket_info
-        @id = socket_info[:id]
-        @socket = nil
+      # @param [Hash] spec
+      # @param [Hash] options
+      # @option options [Debug] :debug
+      def initialize(spec, options = {})
+        @config = spec
+        @id = spec[:id]
         @debug = options[:debug]
+        @socket = nil
       end
 
       # Send a message over the socket
-      # @param [Array<Message>] messages A message or messages to send
+      # @param [Array<::Patch::Message>] messages A message or messages to send
       # @return [String, nil] If a message was sent, its JSON string; otherwise nil
       def puts(messages)
         messages = Array(messages)
@@ -33,6 +37,8 @@ module Patch
         end
       end
 
+      # Start the websocket
+      # @return [Boolean]
       def start
         Thread.abort_on_exception = true
         EM::WebSocket.run(@config) do |websocket| 
@@ -42,15 +48,17 @@ module Patch
             Thread.main.raise(exception)
           end
         end
+        true
       end
 
       private
 
-      # Handle an inputted message
-      # @param [String] raw_message A raw inputted JSON message
+      # Handle a received message
+      # @param [String] json_message A raw inputted JSON message
+      # @param [Proc] callback A callback to fire with the received message
       # @return [Message]
-      def handle_input(raw_message, &block)
-        message_hash = JSON.parse(raw_message, :symbolize_names => true)
+      def handle_input(json_message, &callback)
+        message_hash = JSON.parse(json_message, :symbolize_names => true)
         message = Message.new(:raw_message => message_hash)
         @debug.puts("Recieved message: #{message_hash.to_json}") if @debug
         yield(message) if block_given?
@@ -58,11 +66,16 @@ module Patch
       end
 
       # Enable this server after initializing an EM::Websocket
+      # @param [EM::Websocket] websocket
+      # @return [Boolean]
       def enable(websocket)
         @socket = websocket
         configure
+        true
       end
 
+      # Configure the server actions
+      # @return [Boolean]
       def configure
         @socket.onopen do |handshake|
           puts "Connection open"
@@ -75,7 +88,7 @@ module Patch
         @socket.onmessage do |raw_message|
           handle_input(raw_message, &block)
         end
-
+        true
       end
 
     end
