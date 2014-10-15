ab0bc3563a5bbf237ffbcc3df82808ee5812aa99
Ari Russo <ari.russo@gmail.com>
2014-10-14 20:26:18 -0400

    use top level properties

--- lib/patch/message.rb
+++ lib/patch/message.rb
@@ -6,12 +6,9 @@ module Patch
     attr_accessor :index, :patch_name, :value
     attr_reader :time
 
-    # @param [Hash] options
-    # @option options [Boolean] :debug
-    # @option options [Hash] :raw_message
-    def initialize(options = {})
-      @debug = options[:debug]
-      populate_from_hash(options[:raw_message]) unless options[:raw_message].nil?
+    # @param [Hash] properties
+    def initialize(properties = nil)
+      populate_from_properties(properties) unless properties.nil?
       @time ||= Time.now
     end
 
@@ -43,18 +40,17 @@ module Patch
     private
 
     # Populate this message from a hash of properties
-    # @param [Hash] hash
+    # @param [Hash] properties
     # @return [Hash]
-    def populate_from_hash(hash)
-      hash = hash.dup
-      @index = hash.delete(:index)
-      @patch_name = hash.delete(:patch_name)
-      @value = hash.delete(:value)
-      if !(timestamp = hash.delete(:timestamp)).nil?
+    def populate_from_properties(properties)
+      properties = properties.dup
+      @index = properties.delete(:index)
+      @patch_name = properties.delete(:patch_name)
+      @value = properties.delete(:value)
+      if !(timestamp = properties.delete(:timestamp)).nil?
         @time = timestamp_to_time(timestamp)
       end
-      @other_properties = hash
-      hash
+      @other_properties = properties
     end
 
     # Convert a raw timestamp to a Ruby time
