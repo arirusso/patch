ad5ec43a506c1272940b4afce2a338724ddb371f
Ari Russo <ari.russo@gmail.com>
2014-10-18 13:32:13 -0400

    flexible args

--- lib/patch/patch.rb
+++ lib/patch/patch.rb
@@ -6,12 +6,12 @@ module Patch
     attr_reader :actions, :maps, :name
 
     # @param [Symbol, String] name
-    # @param [Action::Container] actions
-    # @param [Array<Node::Map>, Node::Map] maps
+    # @param [Array<Hash>, Action::Container, Hash] actions An action or actions
+    # @param [Array<Node::Map>, Node::Map] maps A node map or maps
     def initialize(name, actions, maps)
       @name = name
-      @actions = actions
-      @maps = [maps].flatten
+      populate_maps(maps)
+      populate_actions(actions)
     end
 
     # Enable the given nodes to implement this patch
@@ -20,6 +20,32 @@ module Patch
     def enable(nodes)
       result = @maps.map { |map| map.enable(self, nodes) }
       result.any?
+    end
+
+    private
+
+    # Populate the patch actions from various arg formats
+    # @param [Array<Hash>, Action::Container, Hash] actions
+    # @return [Action::Container]
+    def populate_actions(actions)
+      actions = [actions] if actions.kind_of?(Hash)
+      actions = Action::Container.new(actions) if actions.kind_of?(Array)
+      @actions = actions
+    end
+
+    # Populate the node maps from various arg formats
+    # @param [Array<Hash, Node::Map>, Hash, Node::Map] maps
+    # @return [Array<Node::Map>]
+    def populate_maps(maps)
+      maps = [maps] unless maps.kind_of?(Array)
+      maps = maps.map do |map| 
+        if map.kind_of?(Hash)
+          Node::Map.new(maps.keys.first, maps.values.first)
+        else
+          map
+        end
+      end
+      @maps = maps.flatten.compact
     end
 
   end
