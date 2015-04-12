3d000f28c2ea322a917bb1e5321a376abf8e6080
Ari Russo <ari.russo@gmail.com>
2015-04-12 10:42:40 -0400

    use new action api

--- examples/simple/simple.rb
+++ examples/simple/simple.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-$:.unshift File.join( File.dirname( __FILE__ ), '../../lib')
+$:.unshift(File.join("..", "..", "lib"))
 
 require "patch"
 
@@ -9,18 +9,18 @@ osc = Patch::IO::OSC::Server.new(3, 8000, :echo => { :host => "192.168.1.118", :
 
 map = { [midi, osc] => websocket }
 
-action = { 
+action = {
   :name => "Zoom",
   :key => "zoom",
   :default => {
-    :scale => 0.1..5.0
+    :scale => 10..200
   },
   :midi => {
     :channel => 0,
     :index => 1
-  }, 
+  },
   :osc => {
-    :address=>"/1/rotaryA", 
+    :address=>"/1/rotaryA",
     :scale => 0..1.0
   }
 }
