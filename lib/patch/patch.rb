1aeeddc382485f2dc52c7153bcc0b425f200072d
Ari Russo <ari.russo@gmail.com>
2015-05-18 19:48:26 -0400

    extract populate methods

--- lib/patch/patch.rb
+++ lib/patch/patch.rb
@@ -7,11 +7,10 @@ module Patch
 
     # @param [Symbol, String] name
     # @param [Array<Node::Map>, Node::Map] maps A node map or maps
-    # @param [Array<Hash>, Action::Container, Hash] actions An action or actions
+    # @param [Array<Hash>, Hash] actions An action or actions
     def initialize(name, maps, actions)
       @name = name
-      populate_maps(maps)
-      populate_actions(actions)
+      populate(maps, actions)
     end
 
     # Enable the given nodes to implement this patch
@@ -24,13 +23,21 @@ module Patch
 
     private
 
+    # Populate the patch
+    # @param [Array<Hash, Node::Map>, Hash, Node::Map] maps
+    # @param [Array<Hash>, Hash] actions
+    # @return [Patch]
+    def populate(maps, actions)
+      populate_maps(maps)
+      populate_actions(actions)
+      self
+    end
+
     # Populate the patch actions from various arg formats
-    # @param [Array<Hash>, Action::Container, Hash] actions
-    # @return [Action::Container]
+    # @param [Array<Hash>, Hash] actions
+    # @return [Array<Hash>]
     def populate_actions(actions)
-      actions = [actions] if actions.kind_of?(Hash)
-      actions = Action::Container.new(actions) if actions.kind_of?(Array)
-      @actions = actions
+      @actions = actions.kind_of?(Hash) ? [actions] : actions
     end
 
     # Populate the node maps from various arg formats
@@ -38,7 +45,7 @@ module Patch
     # @return [Array<Node::Map>]
     def populate_maps(maps)
       maps = [maps] unless maps.kind_of?(Array)
-      maps = maps.map do |map| 
+      maps = maps.map do |map|
         if map.kind_of?(Hash)
           Node::Map.new(map.keys.first, map.values.first)
         else
