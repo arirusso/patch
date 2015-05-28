387a2aa32b78df0d690eeb07253797f07d1581be
Ari Russo <ari.russo@gmail.com>
2015-05-27 20:00:01 -0400

    test nodes method

--- test/node/map_test.rb
+++ test/node/map_test.rb
@@ -4,22 +4,42 @@ class Patch::Node::MapTest < Minitest::Test
 
   context "Map" do
 
+    setup do
+      load_test_data
+      @nodes = Patch::Config.to_nodes(@nodes_path)
+      @patches = Patch::Config.to_patches(@nodes, @patches_path)
+    end
+
+    context "#nodes" do
+
+      setup do
+        @map = @patches.first.maps.first
+      end
+
+      should "return all from and to nodes" do
+        @nodes = @map.nodes
+        refute_nil @nodes
+        refute_empty @nodes
+        assert @map.from.all? { |node| @nodes.include?(node) }
+        assert @map.to.all? { |node| @nodes.include?(node) }
+      end
+
+    end
+
     context "#enable" do
 
       setup do
-        load_test_data
-        @nodes = Patch::Config.to_nodes(@nodes_path)
-        @patches = Patch::Config.to_patches(@nodes, @patches_path)
         @maps = @patches.first.maps
-        @node_classes = [Patch::IO::MIDI::Input, Patch::IO::OSC::Server, Patch::IO::Websocket::Node]
-        @node_classes.each { |c| c.any_instance.expects(:listen).once }
+        refute_empty @nodes
+        assert [Patch::IO::MIDI::Input, Patch::IO::OSC::Server, Patch::IO::Websocket::Node].all? { |klass| @nodes.map(&:class).include?(klass) }
+        ::Patch::Thread.expects(:new).times(@nodes.count)
       end
 
       teardown do
-        @node_classes.each { |c| c.any_instance.unstub(:listen) }
+        ::Patch::Thread.unstub(:new)
       end
 
-      should "map nodes together" do
+      should "create node start threads" do
         assert @maps.first.enable(@patches.first)
       end
 
