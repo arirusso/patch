a400a15fb1a9966080529c084c96b8a46cbb318b
Ari Russo <ari.russo@gmail.com>
2015-09-28 19:05:37 -0400

    test input filtering

--- test/io/midi/message_test.rb
+++ test/io/midi/message_test.rb
@@ -32,19 +32,60 @@ class Patch::IO::MIDI::MessageTest < Minitest::Test
 
     context ".to_patch_messages" do
 
-      setup do
-        @message = MIDIMessage::ControlChange.new(0, 0, 127)
-        @result = ::Patch::IO::MIDI::Message.to_patch_messages(@patches.first, @message)
+      context "not filtered" do
+
+        setup do
+          @message = MIDIMessage::ControlChange.new(0, 0, 127)
+          @result = ::Patch::IO::MIDI::Message.to_patch_messages(@patches.first, @message)
+        end
+
+        should "have correct values" do
+          refute_nil @result
+          refute_empty @result
+          message = @result.first
+
+          assert_equal ::Patch::Message, message.class
+          assert_equal 0, message.index
+          assert_equal 5, message.value
+        end
+
       end
 
-      should "have correct values" do
-        refute_nil @result
-        refute_empty @result
-        message = @result.first
+      context "filtered" do
 
-        assert_equal ::Patch::Message, message.class
-        assert_equal 0, message.index
-        assert_equal 5, message.value
+        context "pass" do
+
+          setup do
+            @message = MIDIMessage::ControlChange.new(0, 1, 127)
+            @result = ::Patch::IO::MIDI::Message.to_patch_messages(@patches.first, @message)
+          end
+
+          should "have correct values" do
+            refute_nil @result
+            refute_empty @result
+            message = @result.first
+
+            assert_equal ::Patch::Message, message.class
+            assert_equal 1, message.index
+            assert_equal 5, message.value
+          end
+
+        end
+
+        context "reject" do
+
+          setup do
+            @message = MIDIMessage::ControlChange.new(0, 1, 0)
+            @result = ::Patch::IO::MIDI::Message.to_patch_messages(@patches.first, @message)
+          end
+
+          should "have no messages" do
+            refute_nil @result
+            assert_empty @result
+          end
+
+        end
+
       end
 
     end
